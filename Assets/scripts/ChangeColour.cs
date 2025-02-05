f1053d56a769c370272dbd63640e3a7518948074
Owen Roth <rotho@shernet.sheridancollege.ca>
2025-02-05 11:50:34 -0500

    started writing the button code

---
+++ Assets/scripts/ChangeColour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ChangeColour : MonoBehaviour
+{
+    float r;
+    float g;
+    float b;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
